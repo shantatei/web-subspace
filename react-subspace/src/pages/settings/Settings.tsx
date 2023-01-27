4fa4ca7a7dffb60f398869565d09fd076e8bfbad
shanta <teirenxuanshanta@gmail.com>
2023-01-27 17:57:50 +0800

    Made Settings Page Mobile Responsive

--- react-subspace/src/pages/settings/Settings.tsx
+++ react-subspace/src/pages/settings/Settings.tsx
@@ -90,101 +90,98 @@ const Settings = () => {
   };
 
   return (
-    <Container w="2xl">
-      <Box mt="5">
-        <Heading>Settings</Heading>
-
-        <VStack align={"start"}>
-          <Text mt="2" fontSize={{ base: "md", lg: "lg" }}>
-            Profile Picture
-          </Text>
-          <Box
-            bgColor={useColorModeValue("white", "#1d1e1f")}
-            borderRadius="lg"
-            borderWidth="1px"
-            w="100%"
-          >
-            <HStack>
-              <Avatar
-                m={4}
-                border={useColorModeValue(
-                  "4px solid black",
-                  "4px solid var(--chakra-colors-gray-700)"
-                )}
-                size={"xl"}
-                src={AuthUser.user?.profile_image_url}
-              ></Avatar>
-              <VStack align={"center"}>
-                <Input
-                  hidden
-                  type="file"
-                  accept="image/*"
-                  ref={inputRef}
-                  onChange={(event: any | null) => {
-                    const file: File = event.target.files[0];
-                    if (file) {
-                      setImage(file);
-                    } else {
-                      setImage(null);
-                    }
-                  }}
-                />
-                <Button m={2} onClick={() => inputRef.current?.click()}>
-                  Update Profile Picture
-                </Button>
-                <Text fontSize="sm">Must be JPG,PNG,BMP,JPEG</Text>
-              </VStack>
-            </HStack>
-          </Box>
-          <Text mt="2" fontSize={{ base: "md", lg: "lg" }}>
-            Profile Settings
-          </Text>
-          <Box
-            bgColor={useColorModeValue("white", "#1d1e1f")}
-            borderRadius="lg"
-            borderWidth="1px"
-            w="100%"
-          >
-            <VStack
-              as="form"
-              m={2}
-              justifyContent="center"
-              onSubmit={handleSubmit(onSubmitUpdate)}
-            >
-              <FormControl isInvalid={errors.username != null}>
-                <FormLabel>Username</FormLabel>
-                <Input
-                  defaultValue={AuthUser.user?.username}
-                  focusBorderColor={themeColor.primary}
-                  {...register("username", {
-                    required: "Username is Required",
-                  })}
-                />
-                <FormErrorMessage>{errors.username?.message}</FormErrorMessage>
-              </FormControl>
-              <FormControl isInvalid={errors.email != null}>
-                <FormLabel>Email</FormLabel>
-                <Input
-                  defaultValue={AuthUser.user?.email}
-                  focusBorderColor={themeColor.primary}
-                  {...register("email", {
-                    required: "Email is Required",
-                    pattern: {
-                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                      message: "Invalid email address",
-                    },
-                  })}
-                />
-                <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
-              </FormControl>
-              <Button type="submit" w="100%">
-                Save Changes
+    <Container size={{ sm: "xl", md: "2xl" }}>
+      <Heading>Settings</Heading>
+      <VStack align={"start"}>
+        <Text mt="2" fontSize={{ base: "md", lg: "lg" }}>
+          Profile Picture
+        </Text>
+        <Box
+          bgColor={useColorModeValue("white", "#1d1e1f")}
+          borderRadius="lg"
+          borderWidth="1px"
+          w="100%"
+        >
+          <HStack>
+            <Avatar
+              m={4}
+              border={useColorModeValue(
+                "4px solid black",
+                "4px solid var(--chakra-colors-gray-700)"
+              )}
+              size={"xl"}
+              src={AuthUser.user?.profile_image_url}
+            ></Avatar>
+            <VStack align={"center"}>
+              <Input
+                hidden
+                type="file"
+                accept="image/*"
+                ref={inputRef}
+                onChange={(event: any | null) => {
+                  const file: File = event.target.files[0];
+                  if (file) {
+                    setImage(file);
+                  } else {
+                    setImage(null);
+                  }
+                }}
+              />
+              <Button m={2} onClick={() => inputRef.current?.click()}>
+                Update Profile Picture
               </Button>
+              <Text fontSize="sm">Must be JPG,PNG,BMP,JPEG</Text>
             </VStack>
-          </Box>
-          <ChangePassword />
-        </VStack>
-      </Box>
+          </HStack>
+        </Box>
+        <Text mt="2" fontSize={{ base: "md", lg: "lg" }}>
+          Profile Settings
+        </Text>
+        <Box
+          bgColor={useColorModeValue("white", "#1d1e1f")}
+          borderRadius="lg"
+          borderWidth="1px"
+          w="100%"
+        >
+          <VStack
+            as="form"
+            m={2}
+            justifyContent="center"
+            onSubmit={handleSubmit(onSubmitUpdate)}
+          >
+            <FormControl isInvalid={errors.username != null}>
+              <FormLabel>Username</FormLabel>
+              <Input
+                defaultValue={AuthUser.user?.username}
+                focusBorderColor={themeColor.primary}
+                {...register("username", {
+                  required: "Username is Required",
+                })}
+              />
+              <FormErrorMessage>{errors.username?.message}</FormErrorMessage>
+            </FormControl>
+            <FormControl isInvalid={errors.email != null}>
+              <FormLabel>Email</FormLabel>
+              <Input
+                defaultValue={AuthUser.user?.email}
+                focusBorderColor={themeColor.primary}
+                {...register("email", {
+                  required: "Email is Required",
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: "Invalid email address",
+                  },
+                })}
+              />
+              <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
+            </FormControl>
+            <Button type="submit" w="100%">
+              Save Changes
+            </Button>
+          </VStack>
+        </Box>
+        <ChangePassword />
+      </VStack>
       <ImagePreviewModal state={modalState} setState={setModalState} />
     </Container>
   );
