a328cb1132e385459b735869964fac5b6eb0d2a6
shanta <teirenxuanshanta@gmail.com>
2023-02-07 20:36:48 +0800

    Added a fetchhing data state to render progress and fix minor bug

--- react-subspace/src/pages/profile/component/ProfileCommunities.tsx
+++ react-subspace/src/pages/profile/component/ProfileCommunities.tsx
@@ -1,9 +1,9 @@
-import React, { FC, useState, useEffect } from "react";
+import { FC, useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../store";
-import { Community, CommunityUser } from "../../../utils/types";
+import { CommunityUser } from "../../../utils/types";
 import { communityapi } from "../../../api/community";
-import { SimpleGrid, Text, VStack } from "@chakra-ui/react";
+import { SimpleGrid, Text, VStack, Progress } from "@chakra-ui/react";
 import { CommunityCard } from "../../../components/community/CommunityCard";
 
 interface UserCommunities {
@@ -17,64 +17,81 @@ const ProfileCommunities: FC = () => {
     communitedOwned: [],
     communitiesJoined: [],
   });
+  const [fetchingData, setFetchingData] = useState<boolean>(false);
 
   const fetchCommunitiesUser = () => {
+    setFetchingData(true);
     communityapi.get(`communityByUserId/${user?.id}`).then(
       (res) => {
         setCommunitiesUser({
           communitedOwned: res.data.communities_owned,
           communitiesJoined: res.data.communities_joined,
         });
+        setFetchingData(false);
       },
       (error) => {
+        setFetchingData(false);
         console.log(error.response.data);
       }
     );
   };
 
   useEffect(() => {
+    setCommunitiesUser({
+      communitedOwned: [],
+      communitiesJoined: [],
+    });
     fetchCommunitiesUser();
-  }, []);
+  }, [user]);
 
   return (
-    <VStack w="100%" align="start">
-      <Text fontWeight="bold">Communities Owned</Text>
-      <SimpleGrid columns={3} spacing={10} w="100%">
-        {!communitiesUser.communitedOwned.length ? (
-          <Text>This user has yet to owned any community</Text>
-        ) : (
-          communitiesUser.communitedOwned.map(
-            (communityuser: CommunityUser) => {
-              return (
-                <CommunityCard
-                  key={communityuser.id}
-                  community={communityuser.community}
-                  bgColorDark="#1d1e1f"
-                ></CommunityCard>
-              );
-            }
-          )
-        )}
-      </SimpleGrid>
-      <Text fontWeight="bold">Communities Joined</Text>
-      <SimpleGrid columns={3} spacing={10} w="100%">
-        {!communitiesUser.communitiesJoined.length ? (
-          <Text>This user has yet to join any community</Text>
-        ) : (
-          communitiesUser.communitiesJoined.map(
-            (communityuser: CommunityUser) => {
-              return (
-                <CommunityCard
-                  key={communityuser.id}
-                  community={communityuser.community}
-                  bgColorDark="#1d1e1f"
-                ></CommunityCard>
-              );
-            }
-          )
-        )}
-      </SimpleGrid>
-    </VStack>
+    <>
+      {fetchingData == true ? (
+        <Text>
+          <Progress size="xs" isIndeterminate />
+          Fetching Data ...
+        </Text>
+      ) : (
+        <VStack w="100%" align="start">
+          <Text fontWeight="bold">Communities Owned</Text>
+          <SimpleGrid columns={3} spacing={10} w="100%">
+            {!communitiesUser.communitedOwned.length ? (
+              <Text>This user has yet to owned any community</Text>
+            ) : (
+              communitiesUser.communitedOwned.map(
+                (communityuser: CommunityUser) => {
+                  return (
+                    <CommunityCard
+                      key={communityuser.id}
+                      community={communityuser.community}
+                      bgColorDark="#1d1e1f"
+                    ></CommunityCard>
+                  );
+                }
+              )
+            )}
+          </SimpleGrid>
+          <Text fontWeight="bold">Communities Joined</Text>
+          <SimpleGrid columns={3} spacing={10} w="100%">
+            {!communitiesUser.communitiesJoined.length ? (
+              <Text>This user has yet to join any community</Text>
+            ) : (
+              communitiesUser.communitiesJoined.map(
+                (communityuser: CommunityUser) => {
+                  return (
+                    <CommunityCard
+                      key={communityuser.id}
+                      community={communityuser.community}
+                      bgColorDark="#1d1e1f"
+                    ></CommunityCard>
+                  );
+                }
+              )
+            )}
+          </SimpleGrid>
+        </VStack>
+      )}
+    </>
   );
 };
 
