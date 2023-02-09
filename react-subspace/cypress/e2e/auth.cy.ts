b7109dfb30749b13effc16860da2e69887e28aa4
shanta <teirenxuanshanta@gmail.com>
2023-02-09 20:38:05 +0800

    Testing Cypress Automation With Github-Actions

--- react-subspace/cypress/e2e/auth.cy.ts
+++ react-subspace/cypress/e2e/auth.cy.ts
@@ -9,6 +9,7 @@ describe('Auth', () => {
     it('Signup', () => {
 
         cy.signup('testuser', "testuser@gmail.com", "password", "password")
+
     })
 
     it('Login', () => {
@@ -21,29 +22,14 @@ describe('Auth', () => {
 
         cy.login("testuser@gmail.com", "password")
 
-        cy.get('#menu-button-authmenubtn').click();
-
-        cy.contains('Log Out').click()
-
-        cy.contains('Logout Success').should('be.visible')
+        cy.logout()
     })
 
     it('Delete Account', () => {
 
         cy.login("testuser@gmail.com", "password")
 
-        cy.get('#menu-button-authmenubtn').click();
-
-        cy.contains('Settings').click()
-
-        cy.get('#deleteaccform').within(() => {
-            cy.get('input[name=password]').type("password")
-        })
-
-        cy.get('#deleteaccbtn').scrollIntoView()
-        cy.get('#deleteaccbtn').click({force: true})
-
-        cy.contains('Your Account has been deleted').should('be.visible')
+        cy.deleteAccount()
 
     })
 
--- react-subspace/cypress/support/commands.ts
+++ react-subspace/cypress/support/commands.ts
@@ -1,4 +1,5 @@
 import cypress from "cypress";
+import { BASE_URL_DEV } from "../../src/api/api";
 /// <reference types="cypress" />
 
 declare global {
@@ -6,11 +7,19 @@ declare global {
         interface Chainable {
             login(email: string, password: string): Chainable<void>
             signup(username: string, email: string, password: string, confirm_password: string): Chainable<void>
+            logout(): Chainable<void>
+            deleteAccount(): Chainable<void>
         }
     }
 }
 
 Cypress.Commands.add('login', (username, password) => {
+
+    // cy.intercept({
+    //     method: "POST",
+    //     url: BASE_URL_DEV.AUTH + "/login",
+    // }).as("loginResponse");
+
     cy.contains('Login').click()
 
     cy.get('#loginform').within(() => {
@@ -19,10 +28,20 @@ Cypress.Commands.add('login', (username, password) => {
         cy.contains('Log In').click()
     })
 
+    // cy.wait("@loginResponse")
+    //     .its('response.statusCode')
+    //     .should('eq', 200)
+
     cy.contains('Login Success').should('be.visible')
 })
 
 Cypress.Commands.add('signup', (username, email, password, confirm_password) => {
+
+    // cy.intercept({
+    //     method: "POST",
+    //     url: BASE_URL_DEV.AUTH + "/register",
+    // },).as("signupResponse");
+
     cy.contains('Sign Up').click()
 
     cy.get('#signupform').within(() => {
@@ -33,7 +52,46 @@ Cypress.Commands.add('signup', (username, email, password, confirm_password) => 
         cy.contains('Sign Up').click()
     })
 
+    // cy.wait("@signupResponse")
+    //     .its('response.statusCode')
+    //     .should('eq', 200)
+
     cy.contains('Account created.').should('be.visible')
+
+})
+
+Cypress.Commands.add('logout', () => {
+    // cy.intercept({
+    //     method: "POST",
+    //     url: BASE_URL_DEV.AUTH + "/logout",
+    // },).as("logoutResponse");
+
+    cy.get('#menu-button-authmenubtn').click();
+
+    cy.contains('Log Out').click()
+
+    // cy.wait("@logoutResponse")
+    //     .its('response.statusCode')
+    //     .should('eq', 200)
+
+    cy.contains('Logout Success').should('be.visible')
+
+})
+
+Cypress.Commands.add('deleteAccount', () => {
+
+    cy.get('#menu-button-authmenubtn').click();
+
+    cy.contains('Settings').click()
+
+    cy.get('#deleteaccform').within(() => {
+        cy.get('input[name=password]').type("password")
+    })
+
+    cy.get('#deleteaccbtn').scrollIntoView()
+    cy.get('#deleteaccbtn').click({ force: true })
+
+    cy.contains('Your Account has been deleted').should('be.visible')
 
 })
 
